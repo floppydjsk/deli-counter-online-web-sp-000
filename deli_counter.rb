7f66a5d03f12b176be685c7ee227bfdf9a66a86d
Daniel Kim <daniel.kim52@gmail.com>
2020-02-25 12:49:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+katz_deli = []
+def line(katz_deli)
+  if katz_deli.length == 0
+    return "The line is currently empty"
+  else
+    current = "The line is currently: "
+    katz_deli.map.with_index do |customer, index|
+      current << "#{index}. #{customer}"
+    end
+  end
+end
+
+    
